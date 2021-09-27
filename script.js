6f443c8b027797e727d5bfd7466dfd54236a6f85
hieunt0303 <bibi030301@gmail.com>
2021-09-27 21:30:02 +0700

    fix path menu left

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
-document.querySelectorAll('.container-item-tag').forEach(element => {
-    element.onclick = function () {
-        // element.addClass('active')
-    }
-});
 $('.container-item-tag').click(function () {
     $('.container-item-tag').removeClass('active')
+    $('.container-menu').removeClass('active')
     $(this).addClass('active')
+    $('.container-menu').eq([$(this).index()]-1).addClass('active')
+    // console.log($(this).index(), $('.container-menu')[$(this).index()].addClass('active'))
+
 })
+
+function showContainer(index){
+    $('.container-menu').removeClass('active')
+    switch(index){
+        case 1:{
+            
+        }
+    }
+}
